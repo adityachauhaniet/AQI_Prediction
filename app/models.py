bf108bb114c748a413d1358777ec87e91a1c0f3b
adityachauhaniet <adityachauhanietlko22@gmail.com>
2026-03-27 20:58:45 +0530

    fix password length

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(50), nullable = False)
     email = db.Column(db.String(100), unique = True, nullable = False)
-    password = db.Column(db.String(100), nullable = False)
+    password = db.Column(db.String(225), nullable = False)
 
     role = db.Column(db.String(20), default="user")
     #values: "user" | "admin"
